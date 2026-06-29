2a930925eac58c34b2aa93b06be1627a704321c9
tamhuce20022004 <0190967@st.huce.edu.vn>
2026-06-30 00:05:09 +0700

    update access picture

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosClient = axios.create({
   baseURL: "https://sportgo.ddnsfree.com/api",
+  // baseURL: "http://localhost:8000/api",
   headers: {
     "Content-Type": "application/json",
   },
